7d69bf2479f489f7016fc714a001a1461ebd7302
Nathan McRae <me@nathanmcrae.name>
2020-12-21 22:45:21 -0800

    Add pd-midi-mockingbird

---
+++ pd-midi-mockingbird/midimock.c
@@ -0,0 +1,20 @@
+#include <m_pd.h>
+
+typedef struct _midimock {
+    // need something to hold the midi data
+}
+
+void
+midimock_bang(pdobject* obj){
+    // do what you want with the bang
+}
+
+void*
+midimock_new(char* name){
+    // allocate data you need?
+}
+
+void
+midimock_startup(){
+   
+}
