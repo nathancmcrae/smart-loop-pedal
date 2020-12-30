ae29238d6357cafc4b8e6f85257dda4b49e2f06b
Nathan McRae <me@nathanmcrae.name>
2020-12-29 22:32:11 -0800

    Start implementing listening and looping implementation

--- pd-midi-mockingbird/midimock.c
+++ pd-midi-mockingbird/midimock.c
@@ -1,35 +1,97 @@
+#include <assert.h>
 #include <m_pd.h>
 #include <stdbool.h>
 #include <semaphore.h>
 
+#define BUFFER_LEN 500
+
 static t_class *midimock_class;
 
-typedef struct _midimock {
-    t_object obj;
-    //  need something to hold the midi data
+// Note: the first note value will be invalid (-1) because this way the tick index
+// can be aligned with the note/velocity index corresponding to the tick at which
+// that note/velocity was recorded.
+//
+// The first entry in the buffer records the tick at which listening began.
+typedef struct _midibuffer {
+    t_float note[BUFFER_LEN];
+    t_float velocity[BUFFER_LEN];
+    long tick[BUFFER_LEN];
+    // index of the first free slot
+    int index;
+} t_midibuffer;
+
+typedef struct _inputs {
     t_float listen;
     t_float loop;
     t_float note;
-    t_float vel;
+    t_float velocity;
+} t_inputs;
+
+typedef struct _midimock {
+    t_object obj;
+    t_midibuffer buffer;
+    t_inputs in_current;
+    t_inputs in_previous;
     t_outlet *note_out, *vel_out;
     // 1 if loop is ready, 0 otherwise
     t_outlet *loop_ok_out;
-    sem_t busy;
+    bool busy;
+    // do we need to initialize this?
+    long tick;
+    long playback_tick;
+    // the index in the buffer that we are next going to play from
+    int playback_index;
 } t_midimock;
 
 void
 midimock_bang(t_midimock* obj){
-    // don't run when re-entering
-    // I don't think this is what I need for re-entrancy
-    sem_wait(&obj->busy);
+    // don't run when re-entering; not thread-safe
+    //   is it even possible in PD? Isn't the bang callback run synchronously?
+    if(obj->busy) return;
+    obj->busy = true;
+    // done with start-of-function stuff
 
-    // do the stuff
+    // If we're listening, then record the MIDI note every time the inputs change.
+    if(obj->in_current.listen){
+        if(!obj->in_previous.listen){
+            obj->buffer.index = 0;
 
-    sem_post(&obj->busy);
-}
+            obj->buffer.note[0] = -1;
+            obj->buffer.velocity[0] = -1;
+            obj->buffer.tick[0] = obj->tick;
+        } else if(!(obj->in_current.note == obj->in_previous.note
+           && obj->in_current.velocity == obj->in_previous.velocity)){
+            assert(obj->buffer.index < BUFFER_LEN);
 
-// I don't think we will receive floats from the active
-void midimock_float(t_midimock* obj){
+            obj->buffer.note[obj->buffer.index] = obj->in_current.note;
+            obj->buffer.velocity[obj->buffer.index] = obj->in_current.velocity;
+            obj->buffer.tick[obj->buffer.index] = obj->tick;
+
+            obj->buffer.index++;
+        }
+    }
+
+    // If we're looping, then
+    if(obj->in_current.loop){
+
+        if(!obj->in_previous.loop){
+            obj->playback_tick = obj->buffer.tick[0];
+            obj->playback_index = 0;
+        } else if(obj->playback_tick == obj->buffer.tick[obj->playback_index]){
+
+        }
+
+            // if obj->playback_index >= obj->buffer.index, we loop
+
+        obj->playback_tick++;
+    }
+
+    // end-of-function stuff
+    obj->in_previous = obj->in_current;
+
+    obj->tick++;
+
+    obj->busy = false;
 }
 
 void*
@@ -38,10 +100,10 @@ midimock_new(){
    
     //  allocate data you need?
     inlet_new(&x->obj, &x->obj.ob_pd, 0, 0);
-    floatinlet_new(&x->obj, &x->listen);
-    floatinlet_new(&x->obj, &x->loop);
-    floatinlet_new(&x->obj, &x->note);
-    floatinlet_new(&x->obj, &x->vel);
+    floatinlet_new(&x->obj, &x->in_current.listen);
+    floatinlet_new(&x->obj, &x->in_current.loop);
+    floatinlet_new(&x->obj, &x->in_current.note);
+    floatinlet_new(&x->obj, &x->in_current.vel);
 
     // MIDI_Note
     x->note_out = outlet_new(&x->obj, &s_float);
@@ -61,6 +123,5 @@ midimock_startup(){
         CLASS_DEFAULT,
         0);
 
-    class_addfloat(midimock_class, midimock_float);
     class_addbang(midimock_class, midimock_bang);
 }
