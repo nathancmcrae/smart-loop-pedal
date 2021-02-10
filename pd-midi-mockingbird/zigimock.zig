5a4f001a7f9d3995639053f98680870715c13ec7
Nathan McRae <me@nathanmcrae.name>
2021-02-09 16:09:45 -0800

    Finish fleshing out midimock_bang

--- pd-midi-mockingbird/zigimock.zig
+++ pd-midi-mockingbird/zigimock.zig
@@ -34,9 +34,36 @@ export fn midimock_bang(obj: *mock.t_midimock) void {
             obj.buffer.index = obj.buffer.index + 1;
         }
     } else if(obj.in_previous.listen != 0){
-            // mock.post(std.fmt.format("Recorded {} notes", obj.buffer.index));
+        var message_buf: [100]u8 = undefined;
+        const message_slice = message_buf[0..];
+
+        var message = std.fmt.bufPrint(message_slice, "Recorded {} notes", .{ obj.buffer.index });
+        
+        if(message) |actual_message| {
+            mock.post(actual_message.ptr);
+        } else |err| {
+            mock.post("Recorded some notes");
+        }
     }
 
+    if(obj.in_current.loop != 0){
+        if(obj.in_previous.loop == 0) {
+            obj.playback_tick = obj.buffer.tick[0] - 1;
+            obj.playback_index = 0;
+        } else if(obj.playback_tick == obj.buffer.tick[obj.playback_index]){
+            mock.outlet_float(obj.velocity_out, obj.buffer.velocity[obj.playback_index]);
+            mock.outlet_float(obj.note_out, obj.buffer.note[obj.playback_index]);
+
+            obj.playback_index = obj.playback_index + 1;
+
+            if(obj.playback_index >= obj.buffer.index){
+                obj.playback_tick = obj.buffer.tick[0] - 1;
+                obj.playback_index = 0;
+            }
+        }
+    }
+
+    obj.in_previous = obj.in_current;
 
     obj.tick = obj.tick + 1;
     obj.busy = false;
