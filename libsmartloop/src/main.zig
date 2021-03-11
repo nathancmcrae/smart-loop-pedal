3af9129abb3cb583b8301eb4e2e5837e11478508
Nathan McRae <me@nathanmcrae.name>
2021-03-10 21:29:59 -0800

    Start trying to import note files
    Note files will be the main way I will test the zig implementation of looping

--- libsmartloop/src/main.zig
+++ libsmartloop/src/main.zig
@@ -1,24 +1,82 @@
 const std = @import("std");
 
-const midilib = @cImport(@cInclude("midifile.h"));
+const midilib = @cImport({
+    @cInclude("midifile.h");
+    @cInclude("midiinfo.h");
+});
+
+const mecha = @import("mecha")
 
 const os = std.os;
 const Allocator = std.mem.Allocator;
 
 test "nothing to see here" {
     try readMidiFile(std.testing.allocator);
+    std.debug.warn("I'm a statement in a test", .{});
+}
+
+test "allanotherless test " {
+    std.debug.warn("I'm a statement in a test", .{});
 }
 
 pub fn readMidiFile(alloc: *Allocator) !void {
-    const dir: std.fs.Dir = std.fs.cwd();
-    const path = try std.fs.realpathAlloc(alloc, ".");
-    defer alloc.free(path);
-    std.debug.print("cwd: {}", .{path});
+    std.debug.warn("I'm up here", .{});
+    std.debug.warn("Also here", .{});
+    const midi_file = midilib.midiFileOpen("test.mid");
+    std.debug.warn("but not here", .{});
 
-    const file: std.fs.File = try dir.openFile("test.txt", .{.read=true});
-    defer file.close();
+    var msg: midilib.MIDI_MSG = .{
+        .iType = @intToEnum(midilib.tMIDI_MSG, midilib.msgNoteOff),
+        .dt = 0,
+        .dwAbsPos = 0,
+        .iMsgSize = 0,
+        .bImpliedMsg = 0,
+        .iImpliedMsg = @intToEnum(midilib.tMIDI_MSG, midilib.msgNoteOff),
+        .data = 0,
+        .data_sz = 0,
+        .MsgData = .{
+            .NoteOff = .{
+                .iNote = 0,
+                .iChannel = 0,
+            }
+        },
+        .iLastMsgType = @intToEnum(midilib.tMIDI_MSG, midilib.msgNoteOff),
+        .iLastMsgChnl = 0,
+    };
 
-    const reader = file.reader;
+    std.debug.print("I'm here", .{});
+    midilib.midiReadInitMessage(&msg);
+    std.debug.print("I'm there", .{});
+    const iNum = midilib.midiReadGetNumTracks(midi_file);
+    std.debug.print("Num tracks: {}", .{iNum});
+    std.debug.warn("I'm up here", .{});
+}
 
-    const midi_file = midilib.midiFileCreate("test.mid", 1);   
+pub fn readNoteFile(alloc: *Allocator) {
+    const tab = mecha.token(mecha.utf8.char('\t'));
+    const noteOnLine = mecha.combine( . {
+        mecha.token(mecha.string("note_on")),
+        tab,
+        mecha.int(u8, 10),
+        tab,
+        mecha.int(u8, 10),
+        tab,
+        mecha.int(u8, 10),
+        mecha.token(mecha.utf8.char('\n')),
+    });
+
+    const noteOnLine = mecha.combine( . {
+        mecha.token(mecha.string("note_off")),
+        tab,
+        mecha.int(u8, 10),
+        tab,
+        mecha.int(u8, 10),
+        tab,
+        mecha.int(u8, 10),
+        mecha.token(mecha.utf8.char('\n')),
+    });
+    const noteLine = mecha.oneOf(.{noteOnLine, noteOffLine});
+    const noteFileParser = mecha.many(noteLine);
+
+    
 }
