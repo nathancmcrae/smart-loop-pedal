7ca402398075eb8245dbea72335dc3abdc15fc19
Nathan McRae <me@nathanmcrae.name>
2021-03-27 16:47:17 -0700

    Get note file parsing working

--- libsmartloop/src/main.zig
+++ libsmartloop/src/main.zig
@@ -1,82 +1,66 @@
 const std = @import("std");
 
-const midilib = @cImport({
-    @cInclude("midifile.h");
-    @cInclude("midiinfo.h");
-});
-
-const mecha = @import("mecha")
+const mecha = @import("mecha/mecha.zig");
 
 const os = std.os;
 const Allocator = std.mem.Allocator;
 
+const contents = 
+\\note_on	66	185	67
+\\note_on	66	185	67
+\\note_off	67	235	104
+\\note_on	58	406	59
+\\note_off	66	432	101
+\\note_off	50	452	106
+\\note_on	66	548	43
+\\note_off	66	654	96
+\\note_on	66	777	41
+\\note_on	51	784	64
+\\note_off	58	807	101
+;
+
 test "nothing to see here" {
-    try readMidiFile(std.testing.allocator);
-    std.debug.warn("I'm a statement in a test", .{});
+    try readNoteFile(std.testing.allocator, contents);
 }
 
-test "allanotherless test " {
-    std.debug.warn("I'm a statement in a test", .{});
+const tab = mecha.utf8.char('\t');
+const noteLine = mecha.oneOf(.{noteOnLine, noteOffLine});
+const noteFileParser = mecha.many(noteLine, .{.collect = false, .separator = mecha.utf8.char('\n')});
+const noteOnLine = mecha.combine( . {
+    mecha.string("note_on"),
+    tab,
+    mecha.int(u16, 10),
+    tab,
+    mecha.int(u16, 10),
+    tab,
+    mecha.int(u16, 10),
+});
+
+const noteOffLine = mecha.combine( . {
+    mecha.string("note_off"),
+    tab,
+    mecha.int(u16, 10),
+    tab,
+    mecha.int(u16, 10),
+    tab,
+    mecha.int(u16, 10),
+
+});
+
+pub fn readNoteFile(alloc: *Allocator, content: []const u8) !void {
+
+    const a = (try noteFileParser(alloc, content));
+
+    std.debug.warn("parsed content: {}\n", .{a.value});
+    std.debug.warn("unparsed content: {}\n", .{a.rest});
+
+    // for(a) |line, i| {
+    //     std.debug.warn("line {}: {}\n", .{i, line});
+    // }
 }
+ 
+pub fn main() !void {
+    try readNoteFile(std.testing.allocator, contents);
+    std.debug.warn("am run\n", .{});
 
-pub fn readMidiFile(alloc: *Allocator) !void {
-    std.debug.warn("I'm up here", .{});
-    std.debug.warn("Also here", .{});
-    const midi_file = midilib.midiFileOpen("test.mid");
-    std.debug.warn("but not here", .{});
-
-    var msg: midilib.MIDI_MSG = .{
-        .iType = @intToEnum(midilib.tMIDI_MSG, midilib.msgNoteOff),
-        .dt = 0,
-        .dwAbsPos = 0,
-        .iMsgSize = 0,
-        .bImpliedMsg = 0,
-        .iImpliedMsg = @intToEnum(midilib.tMIDI_MSG, midilib.msgNoteOff),
-        .data = 0,
-        .data_sz = 0,
-        .MsgData = .{
-            .NoteOff = .{
-                .iNote = 0,
-                .iChannel = 0,
-            }
-        },
-        .iLastMsgType = @intToEnum(midilib.tMIDI_MSG, midilib.msgNoteOff),
-        .iLastMsgChnl = 0,
-    };
-
-    std.debug.print("I'm here", .{});
-    midilib.midiReadInitMessage(&msg);
-    std.debug.print("I'm there", .{});
-    const iNum = midilib.midiReadGetNumTracks(midi_file);
-    std.debug.print("Num tracks: {}", .{iNum});
-    std.debug.warn("I'm up here", .{});
-}
-
-pub fn readNoteFile(alloc: *Allocator) {
-    const tab = mecha.token(mecha.utf8.char('\t'));
-    const noteOnLine = mecha.combine( . {
-        mecha.token(mecha.string("note_on")),
-        tab,
-        mecha.int(u8, 10),
-        tab,
-        mecha.int(u8, 10),
-        tab,
-        mecha.int(u8, 10),
-        mecha.token(mecha.utf8.char('\n')),
-    });
-
-    const noteOnLine = mecha.combine( . {
-        mecha.token(mecha.string("note_off")),
-        tab,
-        mecha.int(u8, 10),
-        tab,
-        mecha.int(u8, 10),
-        tab,
-        mecha.int(u8, 10),
-        mecha.token(mecha.utf8.char('\n')),
-    });
-    const noteLine = mecha.oneOf(.{noteOnLine, noteOffLine});
-    const noteFileParser = mecha.many(noteLine);
-
-    
 }
